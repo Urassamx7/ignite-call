5081b345959c4cfeb43c01d9e67e272b7c7b01ce
Urassamx7 <marvin.lagas01234@gmail.com>
2026-09-17 11:09:51 +0200

    Add react-hook-form and zod for form validation; update ClaimUsernameForm component to use new validation schema

--- src/modules/home/ui/components/claim-username-form/index.tsx
+++ src/modules/home/ui/components/claim-username-form/index.tsx
@@ -1,7 +1,10 @@
-import { Button, TextInput as IgniteTextInput } from '@ignite-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, TextInput as IgniteTextInput, Text } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import type { ComponentProps, ForwardRefExoticComponent } from 'react'
-import { Form } from './styles'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
 
 type TextInputProps = Omit<ComponentProps<'input'>, 'prefix' | 'size'> & {
 	prefix?: string
@@ -10,18 +13,55 @@ type TextInputProps = Omit<ComponentProps<'input'>, 'prefix' | 'size'> & {
 
 const TextInput = IgniteTextInput as ForwardRefExoticComponent<TextInputProps>
 
+const claimUsernameSchema = z.object({
+	username: z
+		.string('O nome de usuário é obrigatório')
+		.min(3, 'O nome deve ter pelomenos 3 caracteres')
+		.regex(/^([a-z\\-]+)$/i, {
+			message: 'O usuário pode ter apenas letras e hífens',
+		})
+		.transform((username) => username.toLocaleLowerCase()),
+})
+
+type ClaimUsername = z.infer<typeof claimUsernameSchema>
+
 export function ClaimUsernameForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<ClaimUsername>({
+		resolver: zodResolver(claimUsernameSchema),
+		defaultValues: {
+			username: '',
+		},
+	})
+
+	async function handleClaimUsername(data: ClaimUsername) {
+		console.log(data)
+	}
+
 	return (
-		<Form as='form'>
-			<TextInput
-				size='sm'
-				prefix='ignite.com/'
-				placeholder='seu-usuario'
-			/>
-			<Button size='sm' type='submit'>
-				Reservar
-				<ArrowRight />
-			</Button>
-		</Form>
+		<>
+			<Form as='form' onSubmit={handleSubmit(handleClaimUsername)}>
+				<TextInput
+					size='sm'
+					prefix='ignite.com/'
+					placeholder='seu-usuario'
+					{...register('username')}
+				/>
+				<Button size='sm' type='submit'>
+					Reservar
+					<ArrowRight />
+				</Button>
+			</Form>
+			<FormAnnotation>
+				<Text size='sm'>
+					{errors.username
+						? errors.username.message
+						: 'Digite o nome do usuário'}
+				</Text>
+			</FormAnnotation>
+		</>
 	)
 }
