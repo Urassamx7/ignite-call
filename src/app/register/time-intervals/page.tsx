92d037e172eb3b7ef9cf1825da022024584cc000
Urassamx7 <marvin.lagas01234@gmail.com>
2026-09-20 13:06:13 +0200

    Refactor session management in NextAuth configuration to improve user information handling and enhance overall authentication flow.

---
+++ src/app/register/time-intervals/page.tsx
@@ -0,0 +1,5 @@
+import { TimeIntervalsView } from '@/modules/time-intervals/ui/view/time-intervals-view'
+
+export default function Page() {
+	return <TimeIntervalsView />
+}
---
+++ src/modules/time-intervals/ui/view/time-intervals-view.tsx
@@ -0,0 +1,136 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Checkbox, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useFieldArray, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { TextInput } from '@/app/components/text-input'
+import { Container, Header } from '@/modules/register/ui/styles/styles'
+import { getWeekDays } from '@/utils/get-week-days'
+import {
+	IntervalBox,
+	IntervalDay,
+	IntervalInputs,
+	IntervalItem,
+	IntervalsContainer,
+} from '../styles'
+
+const timeIntervalsFormSchema = z.object({})
+type TimeIntervals = z.infer<typeof timeIntervalsFormSchema>
+
+export const TimeIntervalsView = () => {
+	const {
+		control,
+		register,
+		handleSubmit,
+		formState: { isSubmitting, errors },
+	} = useForm({
+		defaultValues: {
+			intervals: [
+				{
+					weekDay: 0,
+					enabled: false,
+					startTime: '08:00',
+					endTime: '17:00',
+				},
+				{
+					weekDay: 1,
+					enabled: true,
+					startTime: '08:00',
+					endTime: '17:00',
+				},
+				{
+					weekDay: 2,
+					enabled: true,
+					startTime: '08:00',
+					endTime: '17:00',
+				},
+				{
+					weekDay: 3,
+					enabled: true,
+					startTime: '08:00',
+					endTime: '17:00',
+				},
+				{
+					weekDay: 4,
+					enabled: true,
+					startTime: '08:00',
+					endTime: '17:00',
+				},
+				{
+					weekDay: 5,
+					enabled: true,
+					startTime: '08:00',
+					endTime: '17:00',
+				},
+				{
+					weekDay: 6,
+					enabled: false,
+					startTime: '08:00',
+					endTime: '17:00',
+				},
+			],
+		},
+	})
+
+	const weekDays = getWeekDays()
+
+	const { fields } = useFieldArray({
+		control,
+		name: 'intervals',
+	})
+
+	async function handleSetTimeIntervals() {}
+
+	return (
+		<Container>
+			<Header>
+				<Heading as='strong'>Quase lá!</Heading>
+				<Text>
+					Defina o intervalo de horários que você está disponível em
+					cada dia da semana.
+				</Text>
+				<MultiStep size={4} currentStep={3} />
+			</Header>
+			<IntervalBox
+				as='form'
+				onSubmit={handleSubmit(handleSetTimeIntervals)}
+			>
+				<IntervalsContainer>
+					{fields.map((field, index) => {
+						return (
+							<IntervalItem key={field.id}>
+								<IntervalDay>
+									<Checkbox />
+									<Text>{weekDays[field.weekDay]}</Text>
+								</IntervalDay>
+								<IntervalInputs>
+									<TextInput
+										size='sm'
+										type='time'
+										step={60}
+										{...register(
+											`intervals.${index}.startTime`
+										)}
+									/>
+									<TextInput
+										size='sm'
+										type='time'
+										step={60}
+										{...register(
+											`intervals.${index}.endTime`
+										)}
+									/>
+								</IntervalInputs>
+							</IntervalItem>
+						)
+					})}
+				</IntervalsContainer>
+				<Button type='submit'>
+					Próximo passo <ArrowRight />
+				</Button>
+			</IntervalBox>
+		</Container>
+	)
+}
